1b7b8691256cc5cd883b922ccae2c6223ea7436e
guguhanogu <guguhanogu@gmail.com>
2021-08-30 18:16:00 +0900

    initial commit

---
+++ memo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memo
+//
+//  Created by 최한호 on 2021/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
