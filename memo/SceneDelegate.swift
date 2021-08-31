9a935b30e965a1871ef1d74ab6c8fc5963ac7c39
guguhanogu <guguhanogu@gmail.com>
2021-08-31 19:54:39 +0900

    Revert "initial settings _test"
    This reverts commit 8f6a6a0950a864db7c63beea3b16793686a070b0.

--- memo/SceneDelegate.swift
+++ memo/SceneDelegate.swift
@@ -14,16 +14,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-         window = UIWindow(windowScene: windowScene)
-         
-         let mainViewController = MemoViewController()
-         
-         let navigationController = UINavigationController(rootViewController: mainViewController)
-                
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
