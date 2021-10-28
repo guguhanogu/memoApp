a50d563f9fcf9d402dc26876d8dd11c4769f4048
guguhanogu <guguhanogu@gmail.com>
2021-10-28 16:00:47 +0900

    CollectionView Demo (1)

---
+++ memo/MemoList/CollectionView/MemoListCollectionViewData.swift
@@ -0,0 +1,13 @@
+//
+//  MemoListCollectionViewData.swift
+//  memo
+//
+//  Created by 최한호 on 2021/10/14.
+//
+
+import Foundation
+import UIKit
+
+struct Data{
+    let title = ["윈터", "카리나", "닝닝", "지젤", "ae윈터", "ae카리나", "ae닝닝", "ae지젤", "윈터", "카리나", "닝닝", "지젤", "ae윈터", "ae카리나", "ae닝닝", "ae지젤", "윈터", "카리나", "닝닝", "지젤", "ae윈터", "ae카리나", "ae닝닝", "ae지젤"]
+}
--- memo/MemoList/MemoListViewController.swift
+++ memo/MemoList/MemoListViewController.swift
@@ -12,13 +12,14 @@ import SnapKit
 
 class MemoListViewController: UIViewController{
     
-    
+    // NavigationButton_Left
     lazy var leftButton: UIBarButtonItem = {
         let button = UIBarButtonItem(title: "정렬", style: .plain, target: self, action: #selector(buttonPressed(_:)))
         
         return button
     }()
     
+    // NavigationButton_Right
     lazy var rightButton: UIBarButtonItem = {
         let button = UIBarButtonItem(title: "새 메모", style: .plain, target: self, action: #selector(secondButtonPressed(_:)))
         
@@ -26,12 +27,18 @@ class MemoListViewController: UIViewController{
     }()
     
     
-    
+    // ListView (삭제예정)
     let listView: MemoListView = {
         let listView = MemoListView()
         return listView
     }()
+    
+    
+    // CollectionView
+    private var memolistCollectionView: MemoListCollectionView!
+    private let data = Data()
 
+    // viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,8 +47,14 @@ class MemoListViewController: UIViewController{
         self.navigationItem.leftBarButtonItem = self.leftButton
         self.navigationItem.rightBarButtonItem = self.rightButton
         
+        /*
         listView.firstButton.addTarget(self, action: #selector(firstButtonPressed(_:)), for: .touchUpInside)
         listView.secondButton.addTarget(self, action: #selector(secondButtonPressed(_:)), for: .touchUpInside)
+        */
+        
+        configureCollectionView()
+        registerCollectionView()
+        collectionViewDelegate()
     }
     
     override func loadView() {
@@ -79,8 +92,46 @@ class MemoListViewController: UIViewController{
         navigationController?.pushViewController(editorViewController, animated: true)
     }
     
+    func configureCollectionView() {
+        memolistCollectionView = MemoListCollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout.init())
+        memolistCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        memolistCollectionView.backgroundColor = .white
+        self.view.addSubview(memolistCollectionView)
+        memolistCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 120).isActive = true
+        memolistCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        memolistCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        memolistCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+    }
+    
+    func registerCollectionView() {
+        memolistCollectionView.register(MemoListCollectionViewCell.classForCoder(), forCellWithReuseIdentifier: "cellIdentifier")
+    }
+    
+    func collectionViewDelegate() {
+        memolistCollectionView.delegate = self
+        memolistCollectionView.dataSource = self
+    }
+    
 }
 
+extension MemoListViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 120, height: 120)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.title.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellIdentifier", for: indexPath) as! MemoListCollectionViewCell
+        
+        cell.titleLabel.text = data.title[indexPath.row]
+        return cell
+    }
+}
 
 
 
